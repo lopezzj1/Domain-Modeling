3bcf0fb47cd0df4214f6304b916fa8c221b3482b
lopezzj1 <lopezzj1@uw.edu>
2015-10-14 19:29:05 -0700

    Initial Commit

---
+++ Domain Modeling/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Domain Modeling
+//
+//  Created by Jill Lopez on 10/14/15.
+//  Copyright © 2015 Jill Lopez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
